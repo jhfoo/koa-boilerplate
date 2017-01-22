9723553fc475c4d6ecfb961c1f5ed0ebed730d67
JH Foo <foojihaw@kungfoo.info>
2017-01-21 21:39:26 -0600

    Error handling enabled

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
   koa = require('koa'),
   KoaStatic = require('koa-static'),
+  router = require('koa-router')(),
   log4js = require('log4js');
 
 // constants
@@ -12,11 +13,53 @@ var app = koa(),
   config = loadConfig(CONFIG_FILENAME);
 
 // koa middlewares
+// logger
+app.use(function* (next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  logger.debug('%s %s - %s', this.method, this.url, ms);
+});
+
+// custom error response
+app.use(function* (next) {
+  try {
+    yield next;
+  } catch (err) {
+    // some errors will have .status
+    // however this is not a guarantee
+    this.status = err.status || 500;
+    this.type = 'html';
+    this.body = '<p>Something <em>exploded</em> (' + err.message + '), please contact someone.</p>';
+
+    // since we handled this manually we'll
+    // want to delegate to the regular app
+    // level error handling as well so that
+    // centralized still functions correctly.
+    this.app.emit('error', err, this);
+  }
+});
+
+// custom responses load here
+router.get('/error', function *(next) {
+  this.body = 'boo';
+  throw new Error('Oh noes!');
+
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// setup static folder
 if (!fs.existsSync(__dirname + '/' + config.WebService.PublicPath)) {
   fs.mkdirSync(__dirname + '/' + config.WebService.PublicPath);
 }
 app.use(KoaStatic(__dirname + '/' + config.WebService.PublicPath));
 logger.info('Public folder: ' + '/' + config.WebService.PublicPath);
+
+// error handler
+app.on('error', function (err) {
+  logger.error(err);
+});
 
 // start listening
 app.listen(config.WebService.ServicePort);
@@ -30,12 +73,12 @@ function loadConfig(filename) {
     encoding: 'utf8'
   });
 
-  return applyDefaults(RawJson === '' ? {} : JSON.parse(RawJson), 
+  return applyDefaults(RawJson === '' ? {} : JSON.parse(RawJson),
     JSON.parse(fs.readFileSync(DEFAULT_CONFIG_FILENAME)));
 }
 
 function applyDefaults(config, defaults) {
-  Object.keys(defaults).forEach ((key) => {
+  Object.keys(defaults).forEach((key) => {
     if (!(key in config))
       config[key] = defaults[key];
     else {
